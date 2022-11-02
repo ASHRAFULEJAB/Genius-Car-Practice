1f092a01a1728156890e55994afd3a20fc139dd9
Md.Ashraful Islam Khan <19103189@iubat.edu>
2022-11-02 06:24:34 +0600

    design-part-service-till-done

---
+++ src/pages/Home/Services/Services.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import ServiceCard from './ServiceCard'
+
+const Services = () => {
+  const [services, setServices] = useState([])
+  useEffect(() => {
+    fetch('services.json')
+      .then((res) => res.json())
+      .then((data) => setServices(data))
+  }, [])
+
+  return (
+    <div>
+      <div className='text-center my-10'>
+        <p className='text-xl font-bold text-orange-700'>Service</p>
+        <h1 className='text-5xl font-bold'>Our Service Area</h1>
+        <p className=''>
+          the majority have suffered alteration in some form, <br /> by injected
+          humour, or randomised words which don't look even slightly believable.{' '}
+        </p>
+      </div>
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-6 lg:grid-cols-3'>
+              {
+                  services.map(service => <ServiceCard
+                      key={service._id}
+                      service={service}
+                  ></ServiceCard>
+                    )
+              }
+      </div>
+    </div>
+  )
+}
+
+export default Services
